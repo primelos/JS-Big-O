cdb66b4d06ab8a1fc65efd7bf97d674ce1661b32
Ale <alearanibar@gmail.com>
2020-08-22 00:35:33 -0700

    leet code

--- javascript-problems/algorithms/searchString.js
+++ javascript-problems/algorithms/searchString.js
@@ -1,5 +1,21 @@
 function naiveSearch(long, pattern){
     
+    let count = 0
+    for(let i = 0; i < long.length; i++){
+        for(let j = 0; j < pattern.length; j++){
+            console.log(`j ${j} i ${i}`)
+            console.log(pattern[j], long[i+j])
+            if(pattern[j] !== long[i+j]){
+                console.log('break')
+                break
+            }
+            if(j === pattern.length -1){
+                console.log('found match')
+                count++
+            }
+        }
+    }
+    return `found ${count} of '${pattern}' in sentence`
 
 }
 
