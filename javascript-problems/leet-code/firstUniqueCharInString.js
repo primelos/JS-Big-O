3c09573212c6465ef19af228b8b6bef6ed457a51
Ale <alearanibar@gmail.com>
2020-09-08 23:59:37 -0700

    unique character

---
+++ javascript-problems/leet-code/firstUniqueCharInString.js
@@ -0,0 +1,29 @@
+// Given a string, find the first non-repeating character in it and return its 
+// index. If it doesn't exist, return -1.
+
+// Examples:
+
+// s = "leetcode"
+// return 0.
+
+// s = "loveleetcode"
+// return 2.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    let hash = {}
+    let count = 1
+
+    for(let i = 0; i < s.length; i++){
+        let letter = s[i]
+        if (hash.hasOwnProperty(letter)){
+            hash[letter] = count + 1
+        } else {
+            hash[letter] = 0
+        }
+    }
+
+}
