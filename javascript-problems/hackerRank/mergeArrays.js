38a02d80882abd78fab7eb25d7521b490c3af843
Ale <alearanibar@gmail.com>
2020-10-01 23:31:44 -0700

    merge array

---
+++ javascript-problems/hackerRank/mergeArrays.js
@@ -0,0 +1,16 @@
+// Implement a program to Merge all the arrays and sort in ascending order.
+
+// Input:
+
+// 3 4
+
+// 5 8 4 7
+
+// 15 32 87 65
+
+// 2 1 78 91
+
+// // 3 - no of arrays to be merged // 4- no of elements in an array
+
+// Output: 1 2 4 5 7 8 15 32 65 78 87 91
+
