2622f315bdf119f03fd3bc833731706138f73b0b
Ale <alearanibar@gmail.com>
2020-11-20 11:30:52 -0800

    frequency better solution

--- javascript-problems/hackerRank/frequencyQuery.js
+++ javascript-problems/hackerRank/frequencyQuery.js
@@ -23,3 +23,42 @@ function freqQuery(queries) {
   // console.log(count, m)
   return result;
 }
+
+
+
+
+//     VERY SMART WAY TO SOLVE THE PROBLEM
+// function freqQuery(arr) {
+//   const result = [];
+//   const hash = {};
+//   const freq = [];
+
+//   for (let i = 0; i < arr.length; i += 1) {
+//     const [action, value] = arr[i];
+//     const initValue = hash[value] || 0;
+//     // console.log(action , value)
+//     if (action === 1) {
+//       hash[value] = initValue + 1;
+//       console.log("initValue", initValue);
+//       //   console.log('hash',hash)
+
+//       freq[initValue] = (freq[initValue] || 0)--;
+//       console.log("freq", freq);
+//       freq[initValue + 1] = (freq[initValue + 1] || 0)++;
+//       console.log("freq2", freq);
+//     }
+
+//     if (action === 2 && initValue > 0) {
+//       hash[value]--;
+
+//       freq[initValue - 1]++;
+
+//       freq[initValue]--;
+//       console.log(freq);
+//     }
+
+//     if (action === 3) result.push(freq[value] > 0 ? 1 : 0);
+//   }
+
+//   return result;
+// }
