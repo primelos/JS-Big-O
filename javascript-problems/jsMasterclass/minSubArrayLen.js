6e78b5c09ed40722a6b9d9c297d372c99c7069f2
Ale <alearanibar@gmail.com>
2020-12-10 00:52:01 -0800

    min sub array len working code

--- javascript-problems/jsMasterclass/minSubArrayLen.js
+++ javascript-problems/jsMasterclass/minSubArrayLen.js
@@ -6,14 +6,34 @@
 
 
 function minsSubArrayLen(arr, num){
+    console.log(arr.length);
+    let total = 0
+    let start = 0
+    let end = 0
+    let minLen = Infinity
 
-    for(let i = 0; i < arr.length; i++){
+    while(start < arr.length){
+        if(total < num && end < arr.length) {
+            total+= arr[end]
+            console.log('before',end);
+            end++
+            console.log('after',end);
 
+            console.log(total)
+        } else if(total >= num){
+            console.log(end);
+            minLen = Math.min(minLen, end-start)
+            total-= arr[start]
+            start++
+        } else {
+            break
+        }
     }
 
-    return 0
+
+    return minLen === Infinity ? 0 : minLen
 }
 
 console.log(minsSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 [4,3]
-console.log(minsSubArrayLen([2,1, 6, 5, 4], 9));  // 2 [5,4]
-console.log(minsSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1 [62] is greater than 52
+// console.log(minsSubArrayLen([2,1, 6, 5, 4], 9));  // 2 [5,4]
+// console.log(minsSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52)); // 1 [62] is greater than 52
