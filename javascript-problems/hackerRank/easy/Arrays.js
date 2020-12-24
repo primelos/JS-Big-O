9588c3244b7a3679d0d7bab0bb8a31b02d97b4fc
Ale <alearanibar@gmail.com>
2020-12-23 23:47:30 -0800

    arrays

---
+++ javascript-problems/hackerRank/easy/Arrays.js
@@ -0,0 +1,16 @@
+// Complete the function reverseArray in the editor below.
+
+// reverseArray has the following parameter(s):
+
+// int A[n]: the array to reverse
+// Returns
+
+// int[n]: the reversed array
+
+
+function reverseArray(a) {
+  return a.reverse();
+}
+
+console.log(reverseArray([4,2,1,7])) 
+// result [7,1,2,4]
