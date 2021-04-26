fad42c79478b617e95711a4fe9acea1f2fc69a40
Ale <alearanibar@gmail.com>
2021-04-25 23:57:07 -0700

    highest product of

--- javascript-problems/interviewCake/mergeRanges.js
+++ javascript-problems/interviewCake/mergeRanges.js
@@ -1,5 +1,36 @@
 function mergeRanges(meetings) {
   // Merge meetings ranges
 
-  return [];
+  const meetingsCopy = JSON.parse(JSON.stringify(meetings));
+
+  const sortedMeetings = meetingsCopy.sort((a, b) => {
+    return a.startTime - b.startTime;
+  });
+
+  const mergedMeetings = [sortedMeetings[0]];
+
+  for (let i = 1; i < sortedMeetings.length; i++) {
+    const currentMeeting = sortedMeetings[i];
+    const lastMergedMeetings = mergedMeetings[mergedMeetings.length - 1];
+   
+    if (currentMeeting.startTime <= lastMergedMeetings.endTime) {
+      lastMergedMeetings.endTime = Math.max(
+        lastMergedMeetings.endTime,
+        currentMeeting.endTime
+      );
+    } else {
+      mergedMeetings.push(currentMeeting);
+    }
+  }
+  return mergedMeetings;
 }
+
+console.log(
+  mergeRanges([
+    { startTime: 0, endTime: 1 },
+    { startTime: 3, endTime: 5 },
+    { startTime: 4, endTime: 8 },
+    { startTime: 10, endTime: 12 },
+    { startTime: 9, endTime: 10 },
+  ])
+);
