150c807fae5a44fdaa184252fe8f027d07d3323a
Ale <alearanibar@gmail.com>
2021-06-20 00:58:54 -0700

    get product of all ints without index O(n)

--- javascript-problems/interviewCake/getProductsOfAllIntsExceptAtIndex.js
+++ javascript-problems/interviewCake/getProductsOfAllIntsExceptAtIndex.js
@@ -20,23 +20,51 @@
 // }
 
 // console.log(getProductsOfAllIntsExceptAtIndex([1,2,3]));
-//
-//
+//[6, 2, 3]
+// This solution is O(n) but it doesn't account for zeros in the array
+// function getProductsOfAllIntsExceptAtIndex(intArray) {
+//   // Make a list of the products
+//   let product = [];
+//   let temp;
+//   let total = 1;
+//   for (let i = 0; i < intArray.length; i++) {
+//     if (intArray[i] !== 0) {
+//       total *= intArray[i];
+//     }
+//   }
+//   console.log(total);
+//   for (let j = 0; j < intArray.length; j++) {
+//     temp = total / intArray[j];
+//     product.push(temp);
+//   }
+//   return product;
+// }
+
+// console.log(getProductsOfAllIntsExceptAtIndex([8, 9, 4, 3, 1, 5]));
+// // [6, 3, 2];
+
+// O(n) time and O(n)O(n) space
 function getProductsOfAllIntsExceptAtIndex(intArray) {
   // Make a list of the products
+  if (intArray.length < 2) throw new Error("need 2 or more in array");
+
   let product = [];
-  let temp;
-  let total = 0;
+  let total = 1;
+
   for (let i = 0; i < intArray.length; i++) {
+    product[i] = total;
+    console.log(product);
     total *= intArray[i];
+    console.log(total);
   }
-  console.log(total);
-  for (let j = 0; j < intArray.length; j++) {
-    temp = total / intArray[j];
-    product.push(temp);
+  console.log(product);
+  total = 1;
+  for (let j = intArray.length - 1; j >= 0; j--) {
+    console.log(product[j]);
+    product[j] *= total;
+    total *= intArray[j];
   }
   return product;
 }
 
-console.log(getProductsOfAllIntsExceptAtIndex([8, 2, 4, 3, 1, 5]));
-// [6, 3, 2];
+console.log(getProductsOfAllIntsExceptAtIndex([8, 9, 0, 3, 1, 5]));
