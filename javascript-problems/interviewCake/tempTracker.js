e608e6c42653bb55c0dc788680d71e67277c4458
Ale <alearanibar@gmail.com>
2021-06-20 23:57:52 -0700

    tempTracker

---
+++ javascript-problems/interviewCake/tempTracker.js
@@ -0,0 +1,46 @@
+// Implement methods to track the max, min, mean, and mode
+
+class TempTracker {
+  constructor() {
+    this.numberOfReadings = 0;
+    this.totalSum = 0;
+    this.mean = null;
+    this.maxTemp = null;
+    this.minTemp = null;
+    this.maxOccurrences = 0;
+  }
+  insert(temperature) {
+    this.numberOfReadings++;
+    this.totalSum += temperature;
+    this.mean = this.totalSum / this.numberOfReadings;
+    // this.maxOccurrences =
+    if (this.maxTemp === null || temperature > this.maxTemp) {
+      this.maxTemp = temperature;
+    }
+    if (this.minTemp === null || temprature < this.minTemp) {
+      this.minTemp = temperature;
+    }
+  }
+
+  getMax() {
+    return this.maxTemp;
+  }
+
+  getMin() {
+    return this.minTemp;
+  }
+
+  getMean() {
+    return this.mean;
+  }
+
+  getMode() {
+    return 0;
+  }
+}
+
+let t = new TempTracker();
+t.insert(90);
+t.insert(70);
+t.insert(45);
+console.log(t);
