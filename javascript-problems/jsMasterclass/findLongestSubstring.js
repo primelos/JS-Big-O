8fc12016060c388796e5e744313663174e535db3
Ale <alearanibar@gmail.com>
2021-08-23 13:01:20 -0700

    binary tree node

--- javascript-problems/jsMasterclass/findLongestSubstring.js
+++ javascript-problems/jsMasterclass/findLongestSubstring.js
@@ -1,22 +1,22 @@
-function findLongestSubstring(str){
-    let longest = 0
-    let seen = {}
-    let start = 0
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
 
-    for(let i = 0; i < str.length; i++){
-        let char = str[i]
-        console.log(char);
-        console.log(seen[char]);
-        if(seen[char]) {
-            start = Math.max(start, seen[char])
-            console.log(start);
-        }
-        console.log(longest);
-        longest = Math.max(longest, i - start + 1)
-        seen[char] = i + 1
-        console.log(seen[char]);
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    console.log(char);
+    console.log(seen[char]);
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+      console.log(start);
     }
-    return longest
+    console.log(longest);
+    longest = Math.max(longest, i - start + 1);
+    seen[char] = i + 1;
+    console.log(seen[char]);
+  }
+  return longest;
 }
 
 // console.log(findLongestSubstring("rithmschool"));
