d4e652c630bd6e70cb1bb6272a52c3b712026a57
Ale <alearanibar@gmail.com>
2022-02-16 23:57:18 -0800

    generateParenthesis done

--- javascript-problems/leet-code/medium/generateParatheses.js
+++ javascript-problems/leet-code/medium/generateParatheses.js
@@ -17,4 +17,28 @@
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function (n) {};
+var generateParenthesis = function (n) {
+  let pos = [];
+
+  function tracking(count, left, right) {
+    console.log("start");
+    if (left == 0 && right == 0) {
+      pos.push(count);
+      console.log("1", count, pos, left, right);
+    }
+
+    if (left > 0) {
+      console.log("2", count, pos, left - 1, right);
+      tracking(count + "(", left - 1, right);
+      console.log("hi");
+    }
+    if (right > left) {
+      console.log("3", count, pos, left, right - 1);
+      tracking(count + ")", left, right - 1);
+    }
+  }
+
+  tracking("", n, n);
+  console.log("out");
+  return pos;
+};
